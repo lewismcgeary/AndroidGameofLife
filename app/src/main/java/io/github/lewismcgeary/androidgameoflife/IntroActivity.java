f90508b0fea6063b96ced216b7eaab5300f0167c
Lewis McGeary <lewismcgeary@gmail.com>
2016-02-03 01:32:34 +0000

    replace intro floating action button with borderless button

--- app/src/main/java/io/github/lewismcgeary/androidgameoflife/IntroActivity.java
+++ app/src/main/java/io/github/lewismcgeary/androidgameoflife/IntroActivity.java
@@ -3,31 +3,28 @@ package io.github.lewismcgeary.androidgameoflife;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.ActivityOptionsCompat;
 import android.support.v4.util.Pair;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.CardView;
-import android.view.MotionEvent;
 import android.view.Surface;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 public class IntroActivity extends AppCompatActivity {
-    FloatingActionButton introDoneButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro);
-        introDoneButton = (FloatingActionButton)findViewById(R.id.intro_done_button);
-        introDoneButton.setOnTouchListener(new View.OnTouchListener() {
+        Button letsPlayButton = (Button)findViewById(R.id.lets_play_button);
+        letsPlayButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public boolean onTouch(View v, MotionEvent event) {
+            public void onClick(View v) {
                 setFixedScreenOrientation();
                 startTransition();
-                return false;
             }
         });
     }
@@ -45,10 +42,9 @@ public class IntroActivity extends AppCompatActivity {
         ImageView logo = (ImageView)findViewById(R.id.app_logo);
         Intent intent = new Intent(this, LifeGameActivity.class);
         Pair<View, String> pair1 = Pair.create((View)introCardView, getString(R.string.card_view_transition_name));
-        Pair<View, String> pair2 = Pair.create((View)introDoneButton, getString(R.string.fab_transition_name));
         Pair<View, String> pair3 = Pair.create((View)logo, getString(R.string.logo_transition_name));
         ActivityOptionsCompat options =
-                ActivityOptionsCompat.makeSceneTransitionAnimation(this, pair1, pair2, pair3);
+                ActivityOptionsCompat.makeSceneTransitionAnimation(this, pair1, pair3);
         ActivityCompat.startActivity(this, intent, options.toBundle());
     }
 
