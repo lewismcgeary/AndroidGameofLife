add368d85a44147e6ebcf4eeb10f3bfe71057493
Lewis McGeary <lewismcgeary@gmail.com>
2016-02-05 11:57:29 +0000

    add AppIntro library and create blank activity

---
+++ app/src/main/java/ExplainRulesActivity.java
@@ -0,0 +1,31 @@
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.github.paolorotolo.appintro.AppIntro2;
+
+/**
+ * Created by Lewis on 05/02/2016.
+ */
+public class ExplainRulesActivity extends AppIntro2 {
+
+
+    @Override
+    public void init(@Nullable Bundle savedInstanceState) {
+
+    }
+
+    @Override
+    public void onDonePressed() {
+
+    }
+
+    @Override
+    public void onNextPressed() {
+
+    }
+
+    @Override
+    public void onSlideChanged() {
+
+    }
+}
