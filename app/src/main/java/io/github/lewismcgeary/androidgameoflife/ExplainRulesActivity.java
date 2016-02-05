9b9a6564843dfe075b31f66ad9607264c9e51504
Lewis McGeary <lewismcgeary@gmail.com>
2016-02-05 12:27:29 +0000

    initialise test fragments in rules viewpager

--- app/src/main/java/io/github/lewismcgeary/androidgameoflife/ExplainRulesActivity.java
+++ app/src/main/java/io/github/lewismcgeary/androidgameoflife/ExplainRulesActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 
 import com.github.paolorotolo.appintro.AppIntro2;
+import com.github.paolorotolo.appintro.AppIntroFragment;
 
 /**
  * Created by Lewis on 05/02/2016.
@@ -13,7 +14,8 @@ public class ExplainRulesActivity extends AppIntro2 {
 
     @Override
     public void init(@Nullable Bundle savedInstanceState) {
-
+        addSlide(AppIntroFragment.newInstance("test", "test description", R.drawable.life_cell_vector, R.color.colorPrimaryDark));
+        addSlide(AppIntroFragment.newInstance("test2", "test description2", R.drawable.life_cell_vector, R.color.colorPrimaryDark));
     }
 
     @Override
