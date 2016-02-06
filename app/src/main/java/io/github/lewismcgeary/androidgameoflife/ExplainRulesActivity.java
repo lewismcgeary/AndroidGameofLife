7f40731d29a2581a7825539fb838abe2249e063a
Lewis McGeary <lewismcgeary@gmail.com>
2016-02-06 00:12:17 +0000

    add higher resolution logo

--- app/src/main/java/io/github/lewismcgeary/androidgameoflife/ExplainRulesActivity.java
+++ app/src/main/java/io/github/lewismcgeary/androidgameoflife/ExplainRulesActivity.java
@@ -15,7 +15,7 @@ public class ExplainRulesActivity extends AppIntro2 {
 
     @Override
     public void init(@Nullable Bundle savedInstanceState) {
-        CustomAppIntroFragment summaryFragment = CustomAppIntroFragment.newInstance(getString(R.string.rules_summary_title), getString(R.string.rules_summary_text), R.drawable.life_logo_no_shadow_96, ContextCompat.getColor(getApplicationContext(), R.color.rulesViewPagerBackground));
+        CustomAppIntroFragment summaryFragment = CustomAppIntroFragment.newInstance(getString(R.string.rules_summary_title), getString(R.string.rules_summary_text), R.drawable.life_logo_no_shadow_368, ContextCompat.getColor(getApplicationContext(), R.color.rulesViewPagerBackground));
         addSlide(summaryFragment);
         addSlide(AppIntroFragment.newInstance(getString(R.string.rules_fewer_than_two_title), getString(R.string.rules_fewer_than_two_text), R.drawable.rules_fewer_than_two_neighbours, ContextCompat.getColor(getApplicationContext(), R.color.rulesViewPagerBackground)));
         addSlide(AppIntroFragment.newInstance(getString(R.string.rules_two_or_three_title), getString(R.string.rules_two_or_three_text), R.drawable.rules_two_or_three_neighbours, ContextCompat.getColor(getApplicationContext(), R.color.rulesViewPagerBackground)));
